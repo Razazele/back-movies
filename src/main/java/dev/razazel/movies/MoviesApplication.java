1e7ebc7dde530a47c0947adf043be4337a66ac46
Razazele <pablo_verdugoj@hotmail.com>
2023-01-24 22:20:54 -0300

    first commit

---
+++ src/main/java/dev/razazel/movies/MoviesApplication.java
@@ -0,0 +1,25 @@
+package dev.razazel.movies;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+
+@SpringBootApplication
+public class MoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+
+
+
+
+
+
+}
